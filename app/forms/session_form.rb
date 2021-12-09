da9b0a503f2cbba6c03954162c65321cfcc868ba
borman73 <zk-borman@yandex.ru>
2021-12-09 10:47:39 +0400

    Indentation corrected

--- app/forms/session_form.rb
+++ app/forms/session_form.rb
@@ -1,25 +1,25 @@
 class SessionForm
-    include ActiveModel::Model
+  include ActiveModel::Model
   
-    attr_accessor(
-      :email,
-      :password
-    )
+  attr_accessor(
+    :email,
+    :password
+  )
   
-    validates :email, presence: true, format: { with: /\A\S+@.+\.\S+\z/ }
-    validates :password, presence: true
-    validate :user_valid?
+  validates :email, presence: true, format: { with: /\A\S+@.+\.\S+\z/ }
+  validates :password, presence: true
+  validate :user_valid?
   
-    def user
-      User.find_by(email: email)
-    end
-  
-    private
-  
-    def user_valid?
-      if user.blank? || !user.authenticate(password)
-        errors.add(:email, "email or password doesn't match" )
-      end
-    end
-  
+  def user
+    User.find_by(email: email)
   end
+  
+  private
+  
+  def user_valid?
+    if user.blank? || !user.authenticate(password)
+      errors.add(:email, "email or password doesn't match" )
+    end
+  end
+  
+end
